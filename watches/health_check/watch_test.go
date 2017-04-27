9d15563f0036cc6acb466cd916a9e666f4a2b07d
Dimitris Bozelos <dbozelos@gmail.com>
2017-04-27 05:26:09 +0000

    ms_watch_health_check Test result preparation function
    Tests whether the result of the Watch is properly prepared depending
    on the response of the healt check request.

--- watches/health_check/watch_test.go
+++ watches/health_check/watch_test.go
@@ -10,6 +10,10 @@ import (
 	"testing"
 
 	// Utilities.
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 	"time"
 
 	// Internal dependencies.
@@ -17,6 +21,11 @@ import (
 	common "github.com/krystalcode/go-mantis-shrimp/watches/common"
 )
 
+/**
+ * Helper types and functions reused in various tests.
+ */
+
+// testWatch generates a Watch object with some defaults.
 func testWatch() Watch {
 	watch := Watch{
 		common.WatchBase{
@@ -28,9 +37,73 @@ func testWatch() Watch {
 		[]int{200},
 		30 * time.Second,
 		[]Condition{},
+		nil,
 		Result{},
 	}
 	return watch
+}
+
+// An HTTP client that returns a response with status 200.
+type MockHTTPClient200 struct{}
+
+func (client MockHTTPClient200) Get(url string) (*http.Response, error) {
+	response := &http.Response{
+		Status: "200 OK",
+		Body:   ioutil.NopCloser(bytes.NewBuffer([]byte{})),
+	}
+
+	return response, nil
+}
+
+// An HTTP client that returns a response with status 400.
+type MockHTTPClient400 struct{}
+
+func (client MockHTTPClient400) Get(url string) (*http.Response, error) {
+	response := &http.Response{
+		Status: "400 BadRequest",
+		Body:   ioutil.NopCloser(bytes.NewBuffer([]byte{})),
+	}
+
+	return response, nil
+}
+
+// An HTTP client that returns an error, simulating an unresponsive URL or a
+// network error.
+type MockHTTPClientError struct{}
+
+func (client MockHTTPClientError) Get(url string) (*http.Response, error) {
+	return nil, fmt.Errorf("cannot reach the given URL within the given timeout")
+}
+
+/**
+ * Test Result preparation depending on the HTTP Response.
+ */
+
+func TestResultPreparation_Success(t *testing.T) {
+	watch := testWatch()
+	client := MockHTTPClient200{}
+	watch.SetHTTPClient(client)
+	watch.data()
+
+	assert.Equal(t, "success", watch.result.Status)
+}
+
+func TestResultPreparation_StatusMismatch(t *testing.T) {
+	watch := testWatch()
+	client := MockHTTPClient400{}
+	watch.SetHTTPClient(client)
+	watch.data()
+
+	assert.Equal(t, "status_mismatch", watch.result.Status)
+}
+
+func TestResultPreparation_Inaccessible(t *testing.T) {
+	watch := testWatch()
+	client := MockHTTPClientError{}
+	watch.SetHTTPClient(client)
+	watch.data()
+
+	assert.Equal(t, "inaccessible", watch.result.Status)
 }
 
 /**
